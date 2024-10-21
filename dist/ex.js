5be42d8331b5d21735cedafa50caee04320eb2a8
Vinicius Matheus <viniciusomagnifico@hotmail.com>
2024-10-21 10:08:11 -0300

    first commit

---
+++ dist/ex.js
@@ -0,0 +1,29 @@
+"use strict";
+
+var alunos = [{
+  nome: 'João',
+  nota: 7.5
+}, {
+  nome: 'Maria',
+  nota: 5.5
+}, {
+  nome: 'Pedro',
+  nota: 8.0
+}, {
+  nome: 'Ana',
+  nota: 6.0
+}, {
+  nome: 'Lucas',
+  nota: 4.0
+}];
+
+// Função para filtrar alunos com nota maior ou igual a 6
+function alunosAprovados(alunos) {
+  return alunos.filter(function (aluno) {
+    return aluno.nota >= 6;
+  });
+}
+
+// Chamando a função e exibindo os alunos aprovados
+var alunosAprovadosList = alunosAprovados(alunos);
+console.log(alunosAprovadosList);
